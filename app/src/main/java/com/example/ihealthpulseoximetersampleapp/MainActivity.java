6a3af5f2b975ce26d62b8479f1eb077ee7b74eb6
milot_aliu <miloti_cs@hotmail.com>
2020-01-29 15:44:22 +0100

    Main activity finished (measurements)

--- app/src/main/java/com/example/ihealthpulseoximetersampleapp/MainActivity.java
+++ app/src/main/java/com/example/ihealthpulseoximetersampleapp/MainActivity.java
@@ -2,23 +2,41 @@ package com.example.ihealthpulseoximetersampleapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.lzyzsd.circleprogress.ArcProgress;
+import com.ihealth.communication.control.Po3Control;
+import com.ihealth.communication.control.PoProfile;
+import com.ihealth.communication.manager.iHealthDevicesCallback;
+import com.ihealth.communication.manager.iHealthDevicesManager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.ic_battery)ImageButton battery;
-    @BindView(R.id.arc_SpO2)ArcProgress oxygenRate;
-    @BindView(R.id.btnStart)Button start;
-    @BindView(R.id.txt_heartBeat)TextView heartBeat;
+    @BindView(R.id.ic_battery)
+    ImageButton batteryBtn;
+    @BindView(R.id.arc_SpO2)
+    ArcProgress oxygenRate;
+    @BindView(R.id.btnStart)
+    Button measureBtn;
+    @BindView(R.id.txt_heartBeat)
+    TextView heartBeat;
 
+    private int mClientCallbackId;
+    private Po3Control mPo3Control;
+
+    private String deviceMac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +44,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        Intent intent = getIntent();
+        deviceMac = intent.getStringExtra("mac");
 
+        mClientCallbackId = iHealthDevicesManager.getInstance().registerClientCallback(miHealthDevicesCallback);
+        iHealthDevicesManager.getInstance().addCallbackFilterForDeviceType(mClientCallbackId, iHealthDevicesManager.TYPE_PO3);
+        mPo3Control = iHealthDevicesManager.getInstance().getPo3Control(deviceMac);
 
+        batteryBtn.setOnClickListener(v -> showBattery());
+        measureBtn.setOnClickListener(v -> startMeasuring());
     }
+
+    private void startMeasuring() {
+        mPo3Control.startMeasure();
+    }
+
+    private void showBattery() {
+        mPo3Control.getBattery();
+    }
+
+    private iHealthDevicesCallback miHealthDevicesCallback = new iHealthDevicesCallback() {
+        @Override
+        public void onDeviceNotify(String mac, String deviceType, String action, String message) {
+            JSONTokener jsonTokener = new JSONTokener(message);
+            switch (action) {
+                case PoProfile.ACTION_LIVEDA_PO:
+                    try {
+                        JSONObject jsonObject = (JSONObject) jsonTokener.nextValue();
+                        int oxygen = jsonObject.getInt(PoProfile.BLOOD_OXYGEN_PO);
+                        int pulseRate = jsonObject.getInt(PoProfile.PULSE_RATE_PO);
+                        oxygenRate.setProgress(oxygen);
+                        heartBeat.setText(pulseRate);
+                        measureBtn.setEnabled(false);
+                        batteryBtn.setEnabled(false);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case PoProfile.ACTION_RESULTDATA_PO:
+                    try {
+                        JSONObject jsonObject = (JSONObject) jsonTokener.nextValue();
+                        String dataId = jsonObject.getString(PoProfile.DATAID);
+                        int oxygen = jsonObject.getInt(PoProfile.BLOOD_OXYGEN_PO);
+                        int pulseRate = jsonObject.getInt(PoProfile.PULSE_RATE_PO);
+                        oxygenRate.setProgress(oxygen);
+                        heartBeat.setText(pulseRate);
+                        measureBtn.setEnabled(false);
+                        batteryBtn.setEnabled(false);
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    break;
+                case PoProfile.ACTION_BATTERY_PO:
+                    JSONObject jsonobject;
+                    try {
+                        jsonobject = (JSONObject) jsonTokener.nextValue();
+                        int battery = jsonobject.getInt(PoProfile.BATTERY_PO);
+                        Toast.makeText(MainActivity.this, battery + "%", Toast.LENGTH_LONG).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    };
 }
